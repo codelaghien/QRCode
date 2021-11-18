56c1448eeb54393e105aaef31f5fe448152b84c2
codelaghien <codelaghien.github.io>
2021-11-18 13:01:05 -0800

    More Info

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   public title = 'QR';
-  public qrInfo = 'https://codelaghien.club/covid19/';
   public width = 300;
+  public baseInfo = 'https://codelaghien.club/covid19';
+  public qrInfo = this.baseInfo;
+  public name = '';
+  public age = 0;
 
-  public change(event: any): void {
-    console.log('event=', event.target.value);
+  constructor(route: ActivatedRoute) {
+    route.queryParams.subscribe((v) => {
+      console.log('queryParams=', v);
+      if (v && v.data) {
+        const data = v.data;
+        console.log('queryParams data =', data);
+        console.log('name =', data.name);
+        console.log('age =', data.age);
+
+        const obj = JSON.parse(data);
+        console.log('queryParams obj =', obj);
+        console.log('name =', obj.name);
+        console.log('age =', obj.age);
+      }
+    });
+  }
+
+  public ngOnInit(): void {}
+
+  public changeBase(event: any): void {
+    this.baseInfo = event.target.value;
     this.qrInfo = event.target.value;
+    this.combine();
+  }
+
+  public changeName(event: any): void {
+    this.name = event.target.value;
+    this.combine();
+  }
+
+  public changeAge(event: any): void {
+    this.age = +event.target.value;
+    this.combine();
+  }
+
+  private combine(): void {
+    const data = JSON.stringify({ name: this.name, age: this.age });
+    this.qrInfo = this.baseInfo + '?data=' + data;
+    console.log('qrInfo=', this.qrInfo);
   }
 }
